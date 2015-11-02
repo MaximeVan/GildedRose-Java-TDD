0d402228c77820c80be168314ac7dc7e14989a0a
vanbossm <maxime.vanbossel@iut-valence.fr>
2015-11-02 17:38:18 +0100

    test JUnit

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -7,9 +7,31 @@ import org.junit.Test;
 public class GildedRoseTest
 {
 	@Test
-	public void aSimpleBeginningTest()
+	public void itemsQualityAndSellInDeacreasePerDayTest()
 	{
-		assertEquals(true,false);
+		Item item = new Item("tata",10,15);
+		GildedRose.updateItem(item);
+		assertEquals(item.getSellIn(),9);
+		assertEquals(item.getQuality(),14);
 	}
-
+	
+	@Test
+	public void ifSellInInfZeroThenSellInDecreaseByOneAndQualityDecreaseByTwoPerDay()
+	{
+		Item item = new Item("toto",-25,16);
+		GildedRose.updateItem(item);
+		assertEquals(item.getSellIn(),-26);
+		assertEquals(item.getQuality(),14);
+	}
+	
+	@Test
+	public void ifQualityUpperOrEqualThanZero()
+	{
+		Item item = new Item("titi",5,3);
+		GildedRose.updateItem(item);
+		assertEquals(item.getQuality(),0);
+	}
+	
 }
+
+
